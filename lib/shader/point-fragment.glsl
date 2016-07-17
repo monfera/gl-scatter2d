4e1b6196e580812cf29a20dbabe41d3698c145d6
Robert Monfera <monfera.robert@gmail.com>
2016-07-17 10:42:44 +0200

    shader improvements

--- lib/shader/point-fragment.glsl
+++ lib/shader/point-fragment.glsl
@@ -3,15 +3,20 @@ precision lowp float;
 uniform vec4 color, borderColor;
 uniform float centerFraction;
 
-float smoothStep(float x, float y) {
-  return 1.0 / (1.0 + exp(50.0*(x - y)));
-}
+varying float simplify;
 
 void main() {
-  float radius = length(2.0*gl_PointCoord.xy-1.0);
-  if(radius > 1.0) {
-    discard;
-  }
-  vec4 baseColor = mix(borderColor, color, smoothStep(radius, centerFraction));
-  gl_FragColor = vec4(baseColor.rgb * baseColor.a, baseColor.a);
+  float radius;
+  vec4 baseColor;
+  if(simplify >= 5.0) {
+    radius = length(2.0*gl_PointCoord.xy-1.0);
+    if(radius > 1.0) {
+      discard;
+    }
+    baseColor = mix(borderColor, color, step(radius, centerFraction));
+    gl_FragColor = vec4(baseColor.rgb * baseColor.a, baseColor.a);
+    } else {
+      gl_FragColor = color;
+    }
+
 }
--- lib/shader/point-vertex.glsl
+++ lib/shader/point-vertex.glsl
@@ -5,8 +5,15 @@ attribute vec2 position;
 uniform mat3 matrix;
 uniform float pointSize;
 
+varying float simplify;
+
 void main() {
   vec3 hgPosition = matrix * vec3(position, 1);
   gl_Position  = vec4(hgPosition.xy, 0, hgPosition.z);
-  gl_PointSize = pointSize;
+  simplify = pointSize;
+  float multiplier = 0.886; // for the same square surface as circle would be
+  if(pointSize >= 5.0) {
+    multiplier = 1.0;
+  }
+  gl_PointSize = pointSize * multiplier;
 }
