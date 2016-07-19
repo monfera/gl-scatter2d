c6127d25833191ecfda6e84297587afff1891e4d
Robert Monfera <monfera.robert@gmail.com>
2016-07-19 16:28:36 +0200

    minor spacing changes

--- lib/shader/point-fragment.glsl
+++ lib/shader/point-fragment.glsl
@@ -18,5 +18,4 @@ void main() {
     } else {
       gl_FragColor = color;
     }
-
 }
--- scatter2d.js
+++ scatter2d.js
@@ -19,9 +19,9 @@ function Scatter2D(plot, offsetBuffer, pickBuffer, shader, pickShader) {
   this.size           = 12.0
   this.borderSize     = 1.0
   this.pointCount     = 0
-  this.color          = [1,0,0,1]
-  this.borderColor    = [0,0,0,1]
-  this.bounds         = [Infinity,Infinity,-Infinity,-Infinity]
+  this.color          = [1, 0, 0, 1]
+  this.borderColor    = [0, 0, 0, 1]
+  this.bounds         = [Infinity, Infinity, -Infinity, -Infinity]
   this.pickOffset     = 0
   this.points         = null
   this.xCoords        = null
@@ -41,6 +41,9 @@ proto.dispose = function() {
 }
 
 proto.update = function(options) {
+
+  var i, j
+
   options = options || {}
 
   function dflt(opt, value) {
@@ -51,9 +54,9 @@ proto.update = function(options) {
   }
 
   this.size         = dflt('size', 12.0)
-  this.color        = dflt('color', [1,0,0,1]).slice()
+  this.color        = dflt('color', [1, 0, 0, 1]).slice()
   this.borderSize   = dflt('borderSize', 1)
-  this.borderColor  = dflt('borderColor', [0,0,0,1]).slice()
+  this.borderColor  = dflt('borderColor', [0, 0, 0, 1]).slice()
 
   //Update point data
   if(this.xCoords) {
@@ -61,25 +64,25 @@ proto.update = function(options) {
   }
   var data          = options.positions
   var packed        = pool.mallocFloat32(data.length)
-  var packedId      = pool.mallocInt32(data.length>>>1)
+  var packedId      = pool.mallocInt32(data.length >>> 1)
   packed.set(data)
   this.points       = data
 
-  this.scales =  [{count: data.length>>>1, offset: 0, pixelSize: 1}]
+  this.scales = [{count: data.length >>> 1, offset: 0, pixelSize: 1}]
   var min = 0
   var max = 10
   this.bounds = [min, min, max, max]
-  for(var i = 0; i < data.length>>>1; i++) {
+  for(i = 0; i < data.length >>> 1; i++) {
     packedId[i] = i
   }
-  for(var i = 0; i < data.length; i++) {
+  for(i = 0; i < data.length; i++) {
     packed[i] = (packed[i] - min) / (max - min)
   }
 
   this.offsetBuffer.update(packed)
   this.pickBuffer.update(packedId)
-  var xCoords      = pool.mallocFloat32(data.length>>>1)
-  for(var i=0,j=0; i<data.length; i+=2,++j) {
+  var xCoords = pool.mallocFloat32(data.length >>> 1)
+  for(i = 0, j = 0; i < data.length; i += 2, ++j) {
     xCoords[j] = packed[i]
   }
   pool.free(packedId)
@@ -92,10 +95,10 @@ proto.update = function(options) {
 }
 
 proto.drawPick = (function() {
-  var MATRIX = [1,0,0,
-                0,1,0,
-                0,0,1]
-  var PICK_VEC4 = [0,0,0,0]
+  var MATRIX = [1, 0, 0,
+                0, 1, 0,
+                0, 0, 1]
+  var PICK_VEC4 = [0, 0, 0, 0]
 return function(pickOffset) {
   var plot          = this.plot
   var shader        = this.pickShader
@@ -121,7 +124,7 @@ return function(pickOffset) {
   var screenX = (viewBox[2] - viewBox[0]) * pixelRatio / plot.pixelRatio
   var screenY = (viewBox[3] - viewBox[1]) * pixelRatio / plot.pixelRatio
 
-  var pixelSize   = Math.min(dataX / screenX, dataY / screenY)
+  var pixelSize = Math.min(dataX / screenX, dataY / screenY)
 
   MATRIX[0] = 2.0 * boundX / dataX
   MATRIX[4] = 2.0 * boundY / dataY
@@ -129,10 +132,10 @@ return function(pickOffset) {
   MATRIX[7] = 2.0 * (bounds[1] - dataBox[1]) / dataY - 1.0
 
   this.pickOffset = pickOffset
-  PICK_VEC4[0] = ( pickOffset      & 0xff)
-  PICK_VEC4[1] = ((pickOffset>>8)  & 0xff)
-  PICK_VEC4[2] = ((pickOffset>>16) & 0xff)
-  PICK_VEC4[3] = ((pickOffset>>24) & 0xff)
+  PICK_VEC4[0] = ( pickOffset        & 0xff)
+  PICK_VEC4[1] = ((pickOffset >> 8)  & 0xff)
+  PICK_VEC4[2] = ((pickOffset >> 16) & 0xff)
+  PICK_VEC4[3] = ((pickOffset >> 24) & 0xff)
 
   shader.bind()
   shader.uniforms.matrix      = MATRIX
@@ -153,7 +156,7 @@ return function(pickOffset) {
   shader.attributes.pickId.pointer(gl.UNSIGNED_BYTE)
 
   for(var scaleNum = scales.length-1; scaleNum >= 0; --scaleNum) {
-    var lod     = scales[scaleNum]
+    var lod = scales[scaleNum]
     if(lod.pixelSize < pixelSize && scaleNum > 1) {
       continue
     }
@@ -194,7 +197,7 @@ proto.draw = (function() {
     var screenX = viewBox[2] - viewBox[0]
     var screenY = viewBox[3] - viewBox[1]
 
-    var pixelSize   = Math.min(dataX / screenX, dataY / screenY)
+    var pixelSize = Math.min(dataX / screenX, dataY / screenY)
 
     MATRIX[0] = 2.0 * boundX / dataX
     MATRIX[4] = 2.0 * boundY / dataY
@@ -208,7 +211,7 @@ proto.draw = (function() {
     shader.uniforms.pointSize   = pixelRatio * (size + borderSize) * Math.sqrt(1 / pixelSize / 100) / 2
 
     if(this.borderSize === 0) {
-      shader.uniforms.centerFraction = 2.0;
+      shader.uniforms.centerFraction = 2.0
     } else {
       shader.uniforms.centerFraction = size / (size + borderSize + 1.25)
     }
@@ -219,7 +222,7 @@ proto.draw = (function() {
     var firstLevel = true
 
     for(var scaleNum = scales.length-1; scaleNum >= 0; --scaleNum) {
-      var lod     = scales[scaleNum]
+      var lod = scales[scaleNum]
       if(lod.pixelSize < pixelSize && scaleNum > 1) {
         continue
       }
@@ -242,21 +245,20 @@ proto.pick = function(x, y, value) {
   var pointId = value - pickOffset
   var points = this.points
   return {
-    object:  this,
+    object: this,
     pointId: pointId,
-    dataCoord: [ points[2*pointId], points[2*pointId+1] ]
+    dataCoord: [points[2 * pointId], points[2 * pointId + 1] ]
   }
 }
 
 function createScatter2D(plot, options) {
-  var gl     = plot.gl
+  var gl = plot.gl
   var buffer = createBuffer(gl)
   var pickBuffer = createBuffer(gl)
   var shader = createShader(gl, SHADERS.pointVertex, SHADERS.pointFragment)
   var pickShader = createShader(gl, SHADERS.pickVertex, SHADERS.pickFragment)
 
-  var result = new Scatter2D(
-    plot, buffer, pickBuffer, shader, pickShader)
+  var result = new Scatter2D(plot, buffer, pickBuffer, shader, pickShader)
   result.update(options)
 
   //Register with plot
